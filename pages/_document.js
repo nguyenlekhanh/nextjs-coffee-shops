e4cdd09ce76c565c283155dd830c7ee875e9df8e
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-07-05 19:21:54 -0500

    init, getStaticProps, getStaticPaths, foursquare, Unsplash, latlong, context

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="preload" href="/fonts/IBMPlexSans-Bold.ttf" as="font" crossOrigin="anonymous"/>
+        <link rel="preload" href="/fonts/IBMPlexSans-Regular.ttf" as="font" crossOrigin="anonymous"/>
+        <link rel="preload" href="/fonts/IBMPlexSans-SemiBold.ttf" as="font" crossOrigin="anonymous"/>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
